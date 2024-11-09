f9231cd096ae9aac769130a266fddb3c239ddb5b
Simone Ceredi <57034077+sceredi@users.noreply.github.com>
2024-11-09 19:16:22 +0100

    feat: add end to end tests (#1)
    * feat: add end to end tests
    
    * fix: wrong sha256 format
    
    * chore: add package-lock
    
    * test: shortened session test
    
    * chore: add comments to tests
    
    * docs: add test comments
    
    ---------
    
    Co-authored-by: Luca Fabri <luca.fabri1999@gmail.com>

---
+++ tests/endToEnd.spec.ts
@@ -0,0 +1,76 @@
+import { expect } from '@playwright/test';
+import { test } from './email';
+
+let isRegistered = false;
+
+test.describe.serial('User workflows', () => {
+  
+  /**
+   * The User can register to the system.
+   */
+  test('registration', async ({ page, email }) => {
+    await page.goto('http://localhost/');
+    await expect(page.locator('#app')).toContainText('LetsStreamIt'); // check if the app is running
+    await expect(page.getByRole('list')).toContainText('Login'); // check if the user is not logged in
+    await page.getByRole('link', { name: 'Login' }).click(); // click on login
+    await page.getByRole('link', { name: 'Create an account' }).click(); // click on create an account
+    await page.getByPlaceholder('Username').fill('test'); // fill the form
+    await page.getByPlaceholder('Email').fill(email);
+    await page.getByPlaceholder('Password', { exact: true }).fill('Password1!');
+    await page.getByPlaceholder('Confirm Password').fill('Password1!');
+    await page.getByRole('button', { name: 'Sign Up' }).click(); // click on sign up
+    await page.waitForTimeout(5000); // Wait for redirect to login
+    await expect(page).toHaveURL('http://localhost/login'); // check if the user is redirected to login
+    await expect(page.getByRole('heading')).toContainText('Please sign in'); // check if the user is on the login page
+
+    isRegistered = true;
+  });
+
+  /**
+   * The user can login to the system, once the registration is completed.
+   */
+  test.beforeEach(async ({ page, email }) => {
+    if (isRegistered) {
+      await page.goto('http://localhost/'); // go to the homepage
+      await page.getByRole('link', { name: 'Login' }).click(); // click on login
+      await page.getByPlaceholder('Email').fill(email); // fill the form
+      await page.getByPlaceholder('Password').fill('Password1!'); 
+      await page.getByRole('button', { name: 'Sign In' }).click(); // click on sign in
+      await page.waitForTimeout(5000); // Wait for redirect to homepage
+      await expect(page).toHaveURL('http://localhost/'); // check if the user is redirected to homepage
+    }
+  });
+
+  /**
+   * The user can logout from the system, if he's logged in.
+   */
+  test('login', async ({ page }) => {
+    await expect(page.getByRole('button')).toContainText('Logout'); // check if the user is logged in
+  });
+
+  /**
+   * The user can:
+   * 1. Successfully create a streaming session, if the provided Youtube video URL is correct;
+   * 2. Join the created streaming session, through an automatic redirection to the dedicated page (taken over by the frontend service);
+   * 3. Send a message to the session chat
+   */
+  test('session', async ({ page }) => {
+    await page.getByText('Create a Streaming Session').click(); // click on create a session
+    await expect(page.locator('form')).toContainText('Create Session'); // check that the create session popup is open
+    await page.getByPlaceholder('Enter Youtube Video URL').fill('https://www.youtube.com/watch?v=jNQXAC9IVRw'); // fill the form
+    await page.getByText('Create Session').click(); // click on create session
+    await page.getByPlaceholder('Message').fill('hello'); // send a message
+    await page.getByRole('button', { name: 'Send Message' }).click(); // click on send message
+  });
+
+  /**
+   * The user can update its profile page
+   */
+  test('profile', async ({ page }) => {
+    await page.getByRole('link', { name: 'Profile' }).click(); // click on profile
+    await page.getByLabel('Bio').fill('bio'); // fill the form
+    await page.getByRole('button', { name: 'Update' }).click(); // click on update
+    await expect(page.getByRole('alert')).toContainText('Profile updated successfully'); // check if the profile is updated
+  });
+});
+
